c0fa79494f60342f788c3611bb8d6a12aa92cdab
victoriaoppel <89945013+victoriaoppel@users.noreply.github.com>
2022-09-10 15:27:53 -0400

    Added Splash Screen

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import SplashScreen from './pages/SplashScreen';
 
 setupIonicReact();
 
@@ -31,8 +32,11 @@ const App: React.FC = () => (
         <Route exact path="/home">
           <Home />
         </Route>
+        <Route exact path="/splashscreen">
+          <SplashScreen />
+        </Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <Redirect to="/splashscreen" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
---
+++ src/pages/SplashScreen.tsx
@@ -0,0 +1,43 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/react';
+import { useEffect, useState } from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+import Cats from '../images/cats.png';
+import Adam from '../images/adam.png';
+import Sand from '../images/sand.png';
+import Window from '../images/window.png';
+import Logo from '../images/logo.png';
+import { url } from 'inspector';
+import 'animate.css';
+
+export default function SplashScreen() {
+
+    const[loaded, setLoaded] = useState(false);
+
+    const[selectedImage, setSelectedImage] = useState(Window);
+    
+    var images = [Cats, Adam, Sand, Window];
+    useEffect(() => {
+        setSelectedImage(images[Math.floor(Math.random()*3)]);
+    }, []);
+
+
+    useEffect(() => {
+         setTimeout(() => {
+             window.location.href = "/home"; 
+         }, 2000);
+    }, []);
+
+  return (
+    <div>
+        {<IonImg src={selectedImage} style={{height:'100vh'}}></IonImg>}
+        <img src={Logo} className="animate__animated animate__fadeIn animate__slow 2s" style={{width: '80%',position: 'absolute', marginTop: '-500px', display:'block', marginLeft:'10%',}}></img>
+
+    </div>
+  );
+
+    
+
+};
+
+
