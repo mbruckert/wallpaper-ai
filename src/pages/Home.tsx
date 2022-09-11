c9741bb1b462ee5e4c8e0039d007fa369b47da62
Hector Alvarez <hectoralvarez@Hectors-MacBook-Pro.local>
2022-09-10 20:08:17 -0400

    fixes to ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, IonButton, IonItemDivider, IonItem, IonSlide, IonSlides, IonCard, IonCardHeader, IonCardContent, IonCardSubtitle, IonCardTitle, IonImg, IonThumbnail, IonIcon} from '@ionic/react';
+import { IonCheckbox,IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, IonButton, IonItemDivider, IonItem, IonSlide, IonSlides, IonCard, IonCardHeader, IonCardContent, IonCardSubtitle, IonCardTitle, IonImg, IonThumbnail, IonIcon} from '@ionic/react';
 import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -15,68 +15,74 @@ const slideOpts = {
 
   return (
     <IonPage>
-        <IonHeader className="ion-no-border">
-     <IonToolbar>
-      <IonTitle>Wallpaper-AI</IonTitle>
-     </IonToolbar>
-    </IonHeader>
+
+      <IonHeader className="ion-no-border">
+        <IonToolbar>
+          <IonTitle>Wallpaper-AI</IonTitle>
+      </IonToolbar>
+      </IonHeader>
+
       <IonContent fullscreen>
-        <IonInput placeholder='Welcome to the #1 Wallpaper app!' />
         <IonItem>
             <IonInput value={text} placeholder="Describe Your Wallpaper" onIonChange={e => setText(e.detail.value!)}></IonInput>
-          </IonItem>
-        
-      </IonContent>
+        </IonItem> 
+      
 
-
-      <IonContent>
     <IonSlides pager={true} options={slideOpts}>
+
       <IonSlide>
         <IonCard>
-            <IonCardHeader style={{textAlign: 'left'}}>
+          <IonCardHeader style={{textAlign: 'left'}}>
             <IonImg src="https://i.ytimg.com/vi/lnocVspafNY/maxresdefault.jpg" />
-              <IonCardSubtitle >Card Subtitle</IonCardSubtitle>
-              <IonCardTitle>Card Title</IonCardTitle>
-            </IonCardHeader>
-
-            <IonCardContent style={{textAlign: 'left'}}>
-              Keep close to Nature's heart... and break clear away, once in awhile,
-              and climb a mountain or spend a week in the woods. Wash your spirit clean.
-            </IonCardContent>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+            <IonItem>
+              <IonCheckbox slot="start"></IonCheckbox>
+            </IonItem>
+            <IonCardTitle>Fantasy</IonCardTitle>
+            </div>
+          </IonCardHeader>
         </IonCard>
       </IonSlide>
+
+
       <IonSlide>
       <IonCard>
             <IonCardHeader style={{textAlign: 'left'}}>
             <IonImg src="https://i.ytimg.com/vi/lnocVspafNY/maxresdefault.jpg" />
-              <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-              <IonCardTitle>Card Title</IonCardTitle>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+            <IonItem>
+              <IonCheckbox slot="start"></IonCheckbox>
+            </IonItem>
+            <IonCardTitle>Photo Realistic</IonCardTitle>
+            </div>
             </IonCardHeader>
-
-            <IonCardContent style={{textAlign: 'left'}}>
-              Keep close to Nature's heart... and break clear away, once in awhile,
-              and climb a mountain or spend a week in the woods. Wash your spirit clean.
-            </IonCardContent>
         </IonCard>
       </IonSlide>
+
+
       <IonSlide>
       <IonCard>
-            <IonCardHeader style={{textAlign: 'left'}}>
+        <IonCardHeader style={{textAlign: 'left'}}>
             <IonImg src="https://i.ytimg.com/vi/lnocVspafNY/maxresdefault.jpg" />
-            
-              <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-              <IonCardTitle>Card Title</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent style={{textAlign: 'left'}}>
-            </IonCardContent>
-        </IonCard>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+            <IonItem>
+              <IonCheckbox slot="start"></IonCheckbox>
+            </IonItem>
+            <IonCardTitle>Animated</IonCardTitle>
+            </div>
+          </IonCardHeader>
+      </IonCard>
       </IonSlide>
+
     </IonSlides>
+
+
       <IonButton href="/result" expand="block">Generate Wallpaper
         <IonIcon slot="start" icon={refresh}/>
       </IonButton>
-  </IonContent>
-    </IonPage>
+
+      </IonContent>
+</IonPage>
     
   );
 };
